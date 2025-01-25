2d0935526e963e9c4d3da43b5009be6291498af3
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-01-25 11:14:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insert an Element at the Bottom of a Stack/insert-an-element-at-the-bottom-of-a-stack.cpp
+++ Difficulty: Easy/Insert an Element at the Bottom of a Stack/insert-an-element-at-the-bottom-of-a-stack.cpp
@@ -11,6 +11,8 @@ using namespace std;
 class Solution{
 public:
     stack<int> insertAtBottom(stack<int> st,int x){
+       
+        
         if(st.empty()){
             st.push(x);
             return st;
@@ -19,9 +21,10 @@ public:
         int temp = st.top();
         st.pop();
         
-        st = insertAtBottom(st, x);
+       st =  insertAtBottom(st, x);
         st.push(temp);
         return st;
+          
     }
 };
 
