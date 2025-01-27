cd127e39a8c9cb96948149b0ba5ba0df9e76a398
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-01-27 21:59:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the Reversals/count-the-reversals.cpp
+++ Difficulty: Medium/Count the Reversals/count-the-reversals.cpp
@@ -21,52 +21,39 @@ cout << "~" << "\n";
 
 int countRev (string s)
 {
-    stack<char>st;
-    int count = 0;
-    
-    if(s.size() & 1){
-        return -1;
-    }
-    
-    for(auto ch : s){
-        if(ch == '{')
-        st.push(ch);
-        else{
-        if(!st.empty() && st.top() == '{'){
-            st.pop();
-        }
-        
-        else{
-            st.push(ch);
-        }
-    }
-    }
-    
-    
+   stack<char>st;
+   if(s.size() & 1){
+       return -1;
+   }
+   
+   for(auto ch : s){
+       if(ch == '{'){
+           st.push(ch);
+       }
+       else{
+           if(!st.empty() && st.top() == '{'){
+               st.pop();
+           }
+           else{
+               st.push(ch);
+           }
+       }
+       
+      
+   } 
+        int ans = 0;
     while(!st.empty()){
-        char a = st.top();
-        st.pop();
-        
-         char b = st.top();
-        st.pop();
-        
-        if(a == b){
-            count += 1;
-        }
-        else{
-            count += 2;
-        }
-    }
-    
-    return count;
-    
+           char a = st.top();
+           st.pop();
+           char b = st.top();
+           st.pop();
+           
+           if(a == b){
+               ans += 1;
+           }
+           else{
+               ans += 2;
+           }
+       }
+       return ans;
 }
-
-
-
-
-
-
-
-
-
