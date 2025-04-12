b4348003dced3d1866f98a9276647702af43695c
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-04-12 23:40:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Josephus problem/josephus-problem.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+/*You are required to complete this method */
+
+class Solution {
+  public:
+  
+  int findwinner(int n, int k){
+       if(n == 1){
+           return 0;
+       }
+       
+       return (findwinner(n - 1,k ) + k) % n;
+  }
+    int josephus(int n, int k) {
+       return findwinner(n, k) + 1;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t; // testcases
+    while (t--) {
+        int n, k;
+        cin >> n >> k; // taking input n and k
+
+        // calling josephus() function
+        Solution ob;
+        cout << ob.josephus(n, k) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
