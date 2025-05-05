b3cf67325d2bfa225a5547d57b60cc24f84594c3
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-05-05 18:21:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in an almost Sorted Array/search-in-an-almost-sorted-array.cpp
+++ Difficulty: Medium/Search in an almost Sorted Array/search-in-an-almost-sorted-array.cpp
@@ -1,7 +1,6 @@
 //{ Driver Code Starts
-#include <iostream>
-#include <sstream>
-#include <vector>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 
@@ -11,29 +10,31 @@ using namespace std;
 class Solution {
   public:
     int findTarget(vector<int>& arr, int target) {
-
-        int n=arr.size();
-        int s=0;
-        int e=n-1;
-        while(s<=e){
-            int mid=s+(e-s)/2;
+        int s = 0;
+        int e = arr.size() - 1;
+        
+        while(s <= e){
+            int mid = s + (e - s) / 2;
             if(arr[mid]==target){
                 return mid;
             }
-            if(mid-1>=s && arr[mid-1]==target){
-                return mid-1;
+            if(mid - 1 >= s && arr[mid - 1] == target){
+                return mid - 1;
             }
-            if(mid+1<=e && arr[mid+1]==target){
-                return mid+1;
+            
+            if(mid + 1 <= e && arr[mid + 1] == target){
+                return mid + 1;
             }
-            if(target<arr[mid]){
-            e=mid-1;
+            
+            if(arr[mid] < target){
+                s = mid + 1;
             }
-            else if(target>arr[mid]){
-                s=mid+1;
+            else{
+                e = mid - 1;
             }
         }
         return -1;
+        
     }
 };
 
