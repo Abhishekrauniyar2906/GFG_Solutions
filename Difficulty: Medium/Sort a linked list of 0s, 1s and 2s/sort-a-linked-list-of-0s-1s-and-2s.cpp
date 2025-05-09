81de7119406da5a0ba1e3a33a0e447725e36af65
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-05-09 23:46:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -45,53 +45,54 @@ void freeList(struct Node* head) {
 };*/
 class Solution {
   public:
+       
+       Node* insertatTail(Node* &tail, Node* &temp){
+           tail -> next = temp;
+           tail = temp;
+       }
     Node* segregate(Node* head) {
-        // code here
-        int zeroCount = 0;
-        int oneCount = 0;
-        int twoCount = 0;
+        Node* zeroHead = new Node(-1);
+        Node* zeroTail = zeroHead;
+        Node* oneHead = new Node(-1);
+        Node* oneTail = oneHead;
+        Node* twoHead = new Node(-1);
+        Node* twoTail = twoHead;
         
         
         Node* temp = head;
         
         while(temp != NULL){
-            if(temp -> data == 0){
-                zeroCount++;
+             int val = temp -> data;
+            if(val == 0){
+                insertatTail(zeroTail, temp);
             }
-            else if(temp -> data == 1){
-                oneCount++;
+            else if(val == 1){
+                insertatTail(oneTail, temp);
             }
-            
-            else{
-                twoCount++;
+            else if(val == 2){
+                insertatTail(twoTail, temp);
             }
-            
-            temp = temp -> next;
-            
-            
-        }
-        
-        
-        temp = head;
-        
-        while(temp != NULL){
-            if(zeroCount != 0){
-                temp -> data = 0;
-                zeroCount--;
-            }
-            
-            else if(oneCount != 0){
-                temp -> data = 1;
-                oneCount--;
-            }
-            
-            else{
-                temp -> data = 2;
-                twoCount--;
-            }
-            
             temp = temp -> next;
         }
+        
+        // merge 3 Linked list
+        
+        if(oneHead -> next != NULL){
+            zeroTail -> next = oneHead -> next;
+        }
+        else{
+            zeroTail -> next = twoHead -> next;
+        }
+        
+        oneTail -> next = twoHead -> next;
+        twoTail -> next = NULL;
+        
+        head = zeroHead -> next;
+        
+        delete zeroHead;
+        delete oneHead;
+        delete twoHead;
+        
         return head;
     }
 };
