bdd2124ce1e4b74e3ea2215f8f48a7c89153b0b2
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-05-25 21:40:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Hands of Straights/hands-of-straights.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    bool isStraightHand(int N, int groupSize, vector<int> &hand) {
+  
+         if (hand.size() % groupSize != 0) return false;
+        map<int,int>freq;
+
+        for(auto num : hand){
+            freq[num]++;
+        }
+        
+        for(auto it : freq){
+            int card = it.first;
+            int count = it.second;
+
+            if(count > 0){
+                for(int i = 0; i < groupSize; i++){
+                    if(freq[card + i] < count){
+                        return false;
+                    }
+                    
+                        freq[card + i] -= count;
+                    
+                }
+            }
+        }
+        return true;
+        
+    
+    }
+};
