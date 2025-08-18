242838f0cc728ca33a135d0a540ebe53f5144a50
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-08-18 10:52:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.cpp
+++ Difficulty: Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.cpp
@@ -1,33 +1,71 @@
 class Solution {
   public:
     double medianOf2(vector<int>& a, vector<int>& b) {
-       int i = 0;
-       int j = 0;
-       
-       vector<int>ans;
-       
-       while(i < a.size() && j < b.size()){
-           if(a[i] <= b[j]){
-               ans.push_back(a[i++]);
-           }
-           else{
-              ans.push_back(b[j++]);
-           }
-       }
-       
-       while(i < a.size()){
-           ans.push_back(a[i++]);
-       }
-       
-        while(j < b.size()){
-           ans.push_back(b[j++]);
-       }
-       int n = ans.size();
-       if(ans.size() % 2 == 1){
-           return ans[n / 2];
-       }
-       else{
-           return (ans[n / 2] + ans[(n / 2) - 1 ] ) / 2.0;
-       }
-    }
+      int i = 0, j = 0, k = 0;
+      
+      int n = a.size();
+      int m = b.size();
+      
+      int size = n + m;
+      
+      int idx1 = (size / 2) - 1;
+      int idx2 = size / 2;
+      
+      int e1 = -1, e2 = -1;
+      
+      while(i < n && j < m){
+          if(a[i] <= b[j]){
+              if(idx1 == k){
+                  e1 = a[i];
+              }
+              
+              if(idx2 == k){
+                  e2 = a[i];
+              }
+              i++;
+          }
+         
+         else{
+             if(idx1 == k){
+                 e1 = b[j];
+             }
+             
+             if(idx2 == k){
+                 e2 = b[j];
+             }
+             j++;
+         } 
+         k++;
+      }
+      
+      while(i < n){
+          if(idx1 == k){
+                  e1 = a[i];
+              }
+              
+              if(idx2 == k){
+                  e2 = a[i];
+              }
+              i++;
+              k++;
+          }
+      
+      
+      while(j < m){
+            if(idx1 == k){
+                 e1 = b[j];
+             }
+             
+             if(idx2 == k){
+                 e2 = b[j];
+             }
+             j++;
+             k++;
+         } 
+      
+      
+      if(size % 2 == 1) return e2;
+      
+      return (e1 + e2) / 2.0;
+    } 
 };
