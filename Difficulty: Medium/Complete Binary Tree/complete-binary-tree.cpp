7959ae95374c62a0d5dbaacd03fa4c303bc06a52
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-09-09 10:29:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Complete Binary Tree/complete-binary-tree.cpp
+++ Difficulty: Medium/Complete Binary Tree/complete-binary-tree.cpp
@@ -1,144 +1,27 @@
-//{ Driver Code Starts
-// A program to check if a given binary tree is complete or not
-#include <bits/stdc++.h>
-using namespace std;
-
-/* A binary tree node has data, pointer to left child
-   and a pointer to right child */
-struct Node {
-    int data;
-    struct Node* left;
-    struct Node* right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-
-Node* buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node* root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current Node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-
-// } Driver Code Ends
-
-
-
 class Solution {
   public:
-  bool solve(Node* tree){
-      
-      if(!tree){
-          return true;
-      }
-        queue<Node*>q;
-        q.push(tree);
+    
+    int nodeCount(Node* root){
+        if(!root) return 0;
         
-        bool notNULL = false;
+        int l = nodeCount(root -> left);
+        int r = nodeCount(root -> right);
         
-        while(!q.empty()){
-            Node* front = q.front();
-            q.pop();
-            
-            if(front == NULL){
-                notNULL = true;
-            }
-            
-            else{
-                if(notNULL){
-                    return false;
-                }
-                
-                q.push(front -> left);
-                q.push(front -> right);
-            }
-        }
-        return true;
+        return l + r + 1;
     }
-    bool isCompleteBT(Node* tree) {
-        // code here
-         return solve(tree);
+    
+    bool isValidCBT(Node* root, int i, int n){
+        if(!root) return true;
+        
+        if(i > n) return false;
+        
+        return isValidCBT(root -> left, 2 * i, n) && isValidCBT(root -> right, 2 * i + 1, n);
+    }
+    bool isCompleteBT(Node* root) {
+        int n = nodeCount(root);
+        int i = 1;
+        
+        return isValidCBT(root, i, n);
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    scanf("%d ", &t);
-    while (t--) {
-        string treeString;
-        getline(cin, treeString);
-        Node* root = buildTree(treeString);
-        Solution ob;
-        if (ob.isCompleteBT(root)) {
-            printf("true\n");
-        } else {
-            printf("false\n");
-        }
-        cout << "~\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
