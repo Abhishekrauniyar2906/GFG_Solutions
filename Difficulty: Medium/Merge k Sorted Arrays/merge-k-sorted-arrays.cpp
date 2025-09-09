016806c5768efc1898f5e0e162203cc863a11cf3
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-09-09 11:45:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
@@ -1,110 +1,47 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include<bits/stdc++.h>
-#define N 105
-using namespace std;
-void printArray(vector<int> arr, int size)
-{
-for (int i=0; i < size; i++)
-	cout << arr[i] << " ";
-}
-
-
-// } Driver Code Ends
-
-class Info{
-    public:
-    
-    int data;
-    int rowIndex;
-    int colIndex;
-    
-    Info(int data, int row, int col){
-        this -> data = data;
-        this -> rowIndex = row;
-        this -> colIndex = col;
-    }
-};
-
-
-class Compare{
-    
-    public:
-    
-    bool operator()(Info* a, Info* b){
-        return a -> data > b -> data;
-    }
-};
-
 class Solution {
   public:
-  
-   void mergeKsortedArrays(vector<vector<int>> arr, int K, vector<int> &ans){
-       priority_queue<Info*, vector<Info*>, Compare> minHeap;
-      
-      for(int i = 0; i < K; i++){
-            int element = arr[i][0];
-            Info* temp = new Info(element, i, 0);
-            minHeap.push(temp);
-      }
-      
-      
-      while(!minHeap.empty()){
-          Info* temp = minHeap.top();
-          minHeap.pop();
-          
-          ans.push_back(temp->data);
-          int topData = temp -> data;
-          int topRow = temp -> rowIndex;
-          int topCol = temp -> colIndex;
-          
-          if(topCol + 1 < arr[topRow].size()){
-              Info* next = new Info(arr[topRow][topCol + 1], topRow, topCol + 1);
-              minHeap.push(next);
-          }
-      }
-   }
+    // Function to merge k sorted arrays.
+    
+    class Info{
+        public:
+        int data;
+        int rowIndex;
+        int ColIndex;
+        Info(int data, int rowIndex, int ColIndex){
+            this -> data = data;
+            this -> rowIndex = rowIndex;
+            this -> ColIndex = ColIndex;
+        }
+    };
+    
+    class Compare{
+        public:
+        bool operator()(Info* a, Info* b){
+            return a -> data > b -> data;
+        }
+    };
     vector<int> mergeKArrays(vector<vector<int>> arr, int K) {
-      
-      vector<int> ans;
-      
-      mergeKsortedArrays(arr, K, ans);
-      return ans;
-        
+       priority_queue<Info*, vector<Info*>, Compare>pq;
+       vector<int>ans;
+       for(int i = 0; i < K; i++){
+           int element = arr[i][0];
+           Info* temp = new Info(element, i, 0);
+           pq.push(temp);
+       }
+        while(!pq.empty()){
+            Info* temp = pq.top();
+            pq.pop();
+            
+            ans.push_back(temp -> data);
+            int topData = temp -> data;
+            int topRow = temp -> rowIndex;
+            int topCol = temp -> ColIndex;
+            
+            if(topCol + 1 < arr[topRow].size()){
+                Info* next = new Info(arr[topRow][topCol + 1], topRow, topCol + 1);
+                pq.push(next);
+            }
+        }
+        return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--){
-	    int k;
-	    cin>>k;
-	    vector<vector<int>> arr(k, vector<int> (k, 0));
-	    for(int i=0; i<k; i++){
-	        for(int j=0; j<k; j++)
-	        {
-	            cin>>arr[i][j];
-	        }
-	    }
-	    Solution obj;
-    	vector<int> output = obj.mergeKArrays(arr, k);
-    	printArray(output, k*k);
-    	cout<<endl;
-    
-cout << "~" << "\n";
-}
-	return 0;
-}
-
-
-
-
-
-
-// } Driver Code Ends
