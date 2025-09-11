0bee1bb0173d034b949aff629e42721771f6d7e1
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-09-11 14:24:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -1,64 +1,26 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-public:
-    int minJumps(vector<int>& nums) {
-        int n = nums.size();
-        if (n <= 1) return 0;
-        if (nums[0] == 0) return -1;
-
+  public:
+    int minJumps(vector<int>& arr) {
+      
+        int n = arr.size();
+        if(n <= 1)return 0;
+        if(arr[0] == 0) return -1;
+        int currEnd = 0;
+        int farthest = 0;
         int jumps = 0;
-        int l = 0, r = 0;
-
-        while (r < n - 1) {
-            int farthest = r;
-            for (int i = l; i <= r; ++i) {
-                farthest = max(farthest, i + nums[i]);
+        
+        for(int i = 0; i < n - 1; i++){
+            farthest = max(farthest, i + arr[i]);
+            
+            
+            if(i == currEnd){
+                jumps++;
+                currEnd = farthest;
+                
+                if(currEnd <= i) return -1;
+                if(currEnd >= n - 1) return jumps;
             }
-
-            if (farthest == r) {
-                // Stuck: Cannot move further
-                return -1;
-            }
-
-            l = r + 1;
-            r = farthest;
-            jumps++;
         }
-
-        return jumps;
+        return -1;
     }
 };
-
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, i, j;
-        vector<int> arr;
-        string ip;
-        int number;
-        getline(cin, ip);
-        stringstream ss(ip);
-
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        cout << obj.minJumps(arr) << endl << "~\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
