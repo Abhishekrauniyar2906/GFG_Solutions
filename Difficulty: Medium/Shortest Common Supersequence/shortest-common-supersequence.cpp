56bb50a09340c8b7c468bee8b293c6d0e6bb8172
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-11-02 19:37:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -6,14 +6,14 @@ class Solution {
     int shortestCommonSupersequence(string &s1, string &s2) {
         int n = s1.size();
         int m = s2.size();
-        vector<vector<int>>dp(n + 1, vector<int>(m + 1));
-        for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= m; j++){
-                if(i == 0 || j == 0){
-                    dp[i][j] = 0;
-                }
-            }
-        }
+        vector<vector<int>>dp(n + 1, vector<int>(m + 1, 0));
+        // for(int i = 0; i <= n; i++){
+        //     for(int j = 0; j <= m; j++){
+        //         if(i == 0 || j == 0){
+        //             dp[i][j] = 0;
+        //         }
+        //     }
+        // }
         
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
