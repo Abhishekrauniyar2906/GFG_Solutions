09de20a8f5d71cf45ef58b0d0e8bcf95575543d0
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-11-02 14:34:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum sum partition/minimum-sum-partition.cpp
+++ Difficulty: Hard/Minimum sum partition/minimum-sum-partition.cpp
@@ -1,46 +1,51 @@
 class Solution {
 
   public:
-  
-   int solve(vector<int> &arr, vector<vector<bool>> &dp, int sum){
-       int n = arr.size();
-       for(int i = 0; i <= n; i++){
-           dp[i][0] = true;
-       }
-       
-       for(int i = 1; i <= n; i++){
-           for(int j = 1; j <= sum; j++){
-               if(arr[i - 1] <= j){
-                   dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
-               }
-               else{
-                   dp[i][j] = dp[i - 1][j];
-               }
-           }
-          
-           }
+    
+    
+    int helper(vector<int> &arr, vector<vector<bool>>&dp, int sum){
+        int n = arr.size();
+        for(int i = 0; i <= arr.size(); i++){
+            dp[i][0] = true;
+        }
+        
+        for(int i = 1; i <= arr.size(); i++){
+            for(int j = 1; j <= sum; j++){
+                if(arr[i - 1] <= j){
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];    
+                }
+                else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+                
+            }
             
-          int max_Subset_sum = 0;
-           for(int j = sum; j >= 0; j--){
-               if(dp[n][j]){
-                   max_Subset_sum = j;
-                   break;
-               }
-           }
-       return max_Subset_sum;
-   }
+        }
+        
+        int maxSubsetSum = 0;
+        
+        for(int j = sum; j >= 0; j--){
+            if(dp[n][j] == true){
+                maxSubsetSum = j;
+                break;
+            }
+        }
+        return maxSubsetSum;
+    }
+    
+    
+    int solve(vector<int> &arr, int sum){
+        int n = arr.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1));
+        int ans = helper(arr, dp, sum);
+        return ans;
+        
+    }
     int minDifference(vector<int>& arr) {
-     int S = accumulate(arr.begin(), arr.end(), 0);
-      int s = S / 2;
-     vector<vector<bool>>dp(arr.size() + 1, vector<bool>(s + 1, 0));
-     
-     int s1 = solve(arr, dp, s);
-     
-     //cout << "S = " << S << ", s1 = " << s1 << ", diff = " << abs(S - 2 * s1) << endl;
-
-     return abs(S - 2 * s1);
+      long long sum = accumulate(arr.begin(), arr.end(), 0);
+      int S = sum / 2;
+      int s = solve(arr, S);
       
-     
-     
+      return abs(sum - 2 * s);
     }
 };
