5da0f2f3a576d0e2a4ddd2db75ddf668f24bc571
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-02-06 15:36:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in an almost Sorted Array/search-in-an-almost-sorted-array.cpp
+++ Difficulty: Medium/Search in an almost Sorted Array/search-in-an-almost-sorted-array.cpp
@@ -1,71 +1,34 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function Template for C++
 class Solution {
   public:
     int findTarget(vector<int>& arr, int target) {
+        int n = arr.size();
         int s = 0;
         int e = arr.size() - 1;
-        
+    
         while(s <= e){
             int mid = s + (e - s) / 2;
-            if(arr[mid]==target){
-                return mid;
-            }
-            if(mid - 1 >= s && arr[mid - 1] == target){
+            
+            if(mid - 1 >= 0 && arr[mid - 1] == target){
                 return mid - 1;
             }
             
-            if(mid + 1 <= e && arr[mid + 1] == target){
+            if(mid + 1 < n && arr[mid + 1] == target){
                 return mid + 1;
             }
             
-            if(arr[mid] < target){
-                s = mid + 1;
+            if(arr[mid] == target){
+                return mid;
+            }
+            
+            else if(arr[mid] < target){
+              
+                s = mid + 2;
             }
             else{
-                e = mid - 1;
+                e = mid - 2;
             }
         }
         return -1;
-        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-
-    while (t-- > 0) {
-        string str;
-        getline(cin, str);
-
-        stringstream ss(str);
-        vector<int> arr;
-        int num;
-        while (ss >> num) {
-            arr.push_back(num);
-        }
-
-        int target;
-        cin >> target;
-        cin.ignore();
-        Solution sln;
-        int ans = sln.findTarget(arr, target);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-// } Driver Code Ends
