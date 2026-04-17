636f8e8e8f25c9d8c91853bbce1262076fdc5d8f
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-04-17 16:59:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subarray with k odds/count-subarray-with-k-odds.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int countSubarrays(vector<int>& arr, int k) {
+        // code here
+        unordered_map<int,int>mp;
+        mp[0] = 1;
+        int csum = 0;
+        int oddCount = 0;
+        
+        int ans = 0;
+        
+        for(int i = 0; i < arr.size(); i++){
+            csum += arr[i];
+            if(arr[i] % 2 != 0){
+                oddCount++;
+            }
+            
+            if(mp.find(oddCount - k) != mp.end()){
+                ans += mp[oddCount - k];
+            }
+        mp[oddCount]++;
+        }
+        return ans;
+    }
+};
