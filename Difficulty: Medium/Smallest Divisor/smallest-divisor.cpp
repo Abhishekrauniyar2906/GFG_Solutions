2f0a6af7d3aeb3ed22a536be983b467f7a3a5897
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-05-06 11:13:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest Divisor/smallest-divisor.cpp
+++ Difficulty: Medium/Smallest Divisor/smallest-divisor.cpp
@@ -1,35 +1,30 @@
 class Solution {
   public:
   
-    bool isPossible(vector<int>& arr, int k, int mid){
+    bool isPossible(vector<int>&arr, int mid, int k){
         int sum = 0;
         
-        for(int i = 0; i < arr.size(); i++){
-             sum += ceil((double)arr[i] / mid);
-
+        for(auto x : arr){
+            sum += ceil((double)x / mid);
         }
-        
         return sum <= k;
-      
     }
     int smallestDivisor(vector<int>& arr, int k) {
-      
-      
-       int s = 1;
-       int e = *max_element(arr.begin(), arr.end());
-       
-       int ans = -1;
-       
-       while(s <= e){
-           int mid = s + (e - s) / 2;
-           if(isPossible(arr, k, mid)){
-               ans = mid;
-               e = mid - 1;
-           }
-           else{
-               s = mid + 1;
-           }
-       }
-       return ans;
+        // Code here
+        int s = 1;
+        int e = *max_element(arr.begin(), arr.end());
+        int ans = -1;
+        while(s <= e){
+            int mid  = s + (e - s) / 2;
+            
+            if(isPossible(arr, mid, k)){
+                ans = mid;
+                e = mid - 1;
+            }
+            else{
+                s = mid + 1;
+            }
+        }
+        return ans;
     }
 };
