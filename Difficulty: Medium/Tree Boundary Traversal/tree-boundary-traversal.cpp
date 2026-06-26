34ab14e80ecd263f11cca5bebd15393d673306f0
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-06-26 16:24:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
@@ -2,59 +2,60 @@
 
 class Solution {
   public:
-  void leftBoundary(Node* root, vector<int>&ans){
-      if(!root) return;
-      
-      if(root -> left == NULL && root -> right == NULL) return; 
-      
-      ans.push_back(root -> data);
-      if(root -> left != NULL){
-          leftBoundary(root -> left, ans);
-      }
-      else{
-          leftBoundary(root -> right, ans);
-      }
-  }
   
-  void leafBoundary(Node* root, vector<int> &ans){
-      if(!root) return;
-      
-      if(root -> left == NULL && root -> right == NULL){
-         ans.push_back(root -> data);  
-      }
-        leafBoundary(root -> left, ans);
-        leafBoundary(root -> right, ans);
-      
-  }
-  
-    void rightBoundary(Node* root, vector<int>&ans){
-      if(!root) return;
-      
-      if(root -> left == NULL && root -> right == NULL) return; 
-      
-      
-      if(root -> right != NULL){
-          rightBoundary(root -> right, ans);
-      }
-      else{
-          rightBoundary(root -> left, ans);
-      }
-      ans.push_back(root -> data);
-  }
-  
-    void solve(Node* root, vector<int> &ans){
+    void leftBoundary(Node* root, vector<int>&ans){
         if(!root) return;
-        ans.push_back(root ->data);
-        leftBoundary(root -> left, ans);
-        leafBoundary(root -> left, ans);
-        leafBoundary(root -> right, ans);
-        rightBoundary(root -> right, ans);
         
+        if(root -> left == NULL && root -> right == NULL) return;
+        ans.push_back(root -> data);
+        
+        if(root -> left != NULL){
+             leftBoundary(root -> left, ans);
+        }
+        else{
+          leftBoundary(root -> right, ans);   
+        }
+       
+       
     }
-    vector<int> boundaryTraversal(Node *root) {
-        vector<int>ans;
+    
+     void leafBoundary(Node* root, vector<int>&ans){
+        if(!root) return;
+        
+        if(root -> left == NULL && root -> right == NULL){
+        ans.push_back(root -> data);
+        }
+        
       
-      solve(root, ans);
-      return ans;
+             leafBoundary(root -> left, ans);
+
+          leafBoundary(root -> right, ans);   
+        }
+    
+    
+    
+     void rightBoundary(Node* root, vector<int>&ans){
+        if(!root) return;
+        
+        if(root -> left == NULL && root -> right == NULL)return;
+       
+        if(root -> right != NULL) rightBoundary(root -> right, ans);
+
+         else rightBoundary(root -> left, ans);   
+        
+       ans.push_back(root -> data);
+       
+    }
+       vector<int> boundaryTraversal(Node *root) {
+     vector<int>ans;
+     if(!root) return ans;
+     ans.push_back(root -> data);
+     leftBoundary(root -> left, ans);
+     leafBoundary(root -> left, ans);
+     leafBoundary(root -> right, ans);
+     rightBoundary(root -> right, ans);
+     
+     return ans;
+        
     }
 };
