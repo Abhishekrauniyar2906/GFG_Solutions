d24ec4cbc69b03d8fbbdc05c27a531e4c8f3be59
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-07-26 17:40:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -1,20 +1,13 @@
 class Solution {
   public:
-    int lcs(string &s1, string &s2) {
-        int n = s1.size();
-        int m = s2.size();
+    int lcs(string &a, string &b) {
+        // code here
+           int n = a.size(), m = b.size();
+        vector<vector<int>>dp(n + 1, vector<int>(m + 1, 0));
         
-        vector<vector<int>>dp(n + 1, vector<int>(m + 1));
-        for(int i = 0; i <=n; i++){
-            for(int j = 0; j <= m; j++){
-                if(i == 0 || j == 0)
-                dp[i][j] = 0;
-            }
-        }
-        
-        for(int i = 1; i <=n; i++){
+        for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
-                if(s1[i - 1] == s2[j - 1]){
+                if(a[i - 1] == b[j - 1]){
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 }
                 else{
@@ -23,5 +16,6 @@ class Solution {
             }
         }
         return dp[n][m];
+        
     }
 };
