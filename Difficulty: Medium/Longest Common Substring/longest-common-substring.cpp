5ab742eaebc4c1af10f5680a2a288db8eb588328
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-07-26 17:42:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -1,32 +1,24 @@
 class Solution {
   public:
-    int longestCommonSubstr(string& s, string& k) {
-     int n = s.size();
-     int m = k.size();
-     
-     vector<vector<int>>dp(n + 1, vector<int>(m + 1));
-      int res = INT_MIN;
-     for(int i = 0; i <= n; i++){
-         
-             dp[i][0] = 0;
-         }
-         
-           for(int j = 0; j <= m; j++){
-             dp[0][j] = 0;
-         }
-         
-         for(int i = 1; i <= n; i++){
-             for(int j = 1; j <= m; j++){
-                 if(s[i - 1] == k[j - 1]){
-                     dp[i][j] = 1 + dp[i - 1][j - 1];
-                 }
-                 else{
-                     dp[i][j] = 0;
-                 }
-                 
-                 res = max(res, dp[i][j]);
-             }
-         }
-         return res;
+    int longCommSubstr(string& a, string& b) {
+        // code here
+           int n = a.size(), m = b.size();
+        vector<vector<int>>dp(n + 1, vector<int>(m + 1, 0));
+        int maxi = INT_MIN;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(a[i - 1] == b[j - 1]){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] = 0;
+                }
+                maxi = max(maxi, dp[i][j]);
+            }
+            
+        }
+        return maxi;
+        
     }
 };
